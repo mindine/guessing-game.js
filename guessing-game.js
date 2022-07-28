d9049ac8d9b1d70e9767ec6e891d7c0d4d0eb99f
mindine <agheutatoo@gmail.com>
2022-07-27 22:51:13 -0500

    finalized game

--- guessing-game.js
+++ guessing-game.js
@@ -13,11 +13,11 @@ const rl = readline.createInterface({
 //Guessing game project
 //phase one tooHigh? tooLow? whoKnows
 
-let secretNumber = 55; //initializing a variable in the global scope 
+let secretNumber = randomInRange(0, 100); //initializing a variable in the global scope 
 
-//checkGuess
+//checkGuess function
 
-let checkGuess = (number) => {
+function checkGuess(number) {
 
   let num = Number(number);
   if (num > secretNumber) { //output if guess is higher than secretNumber
@@ -37,9 +37,9 @@ let checkGuess = (number) => {
 
 }
 
-//askGuess
+//askGuess function
 
-let askGuess = () => {
+function askGuess() {
   rl.question('Enter a guess: ', (answer) => {
     // TODO: Log the answer in a database
     let check = checkGuess(answer);
@@ -53,6 +53,35 @@ let askGuess = () => {
   });
 }
 
-askGuess();
+// askGuess(); //calling function in global space
 
+//randomInRange function
 
+function randomInRange(min, max) {
+
+  min = Math.ceil(min);
+
+  max = Math.floor(max);
+
+  return Math.floor(Math.random() * (max - min) + min); //the max and min are inclusive;
+
+}
+
+//askRange function
+
+function askRange(min, max) {
+
+  rl.question('Enter a min number: ', (answer1) => {
+    // TODO: Log the answer in a database
+    if (answer1 >= 0 || answer1 < 0) {                   //condition to ask for max number if and postive or negative number as been chosen for min number
+      rl.question(`Enter a max number: `, (answer2) => {
+        console.log(`I am thinking of a number between ${answer1} and ${answer2}...`);
+        secretNumber = randomInRange(answer1, answer2); //set secretNumber to min and max given by user
+        askGuess();                                     //run guess function to find the random secret number once found exit game
+      })
+    }
+  });
+
+}
+
+askRange();
